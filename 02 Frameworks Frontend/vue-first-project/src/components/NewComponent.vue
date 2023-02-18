5a10130d71449ff02e16394a64566aae5f696e77
Cristian Melo <cristianmeelo@gmail.com>
2023-02-18 10:01:00 -0300

    :sparkles: feat: class 02 Frameworks Frontend

---
+++ 02 Frameworks Frontend/vue-first-project/src/components/NewComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Este é um componente novo!</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
